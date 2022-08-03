11a63996ae6f5f629155d84383b53ee9b956e10e
Philipp da Silva Fonseca <philipp.fonseca@rwth-aachen.de>
2022-08-03 20:31:32 +0200

    Added Listener for testing.

--- src/main/java/mcchaosplugin/mcchaosplugin/McChaosPlugin.java
+++ src/main/java/mcchaosplugin/mcchaosplugin/McChaosPlugin.java
@@ -1,17 +1,35 @@
 package mcchaosplugin.mcchaosplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.*;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.EventExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public final class McChaosPlugin extends JavaPlugin {
+public final class McChaosPlugin extends JavaPlugin implements Listener, EventExecutor {
 
     @Override
     public void onEnable() {
         Bukkit.getLogger().info("Hello overworld");
+        Bukkit.getPluginManager().registerEvent(PlayerRespawnEvent.class, this, EventPriority.NORMAL, this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onRespawn(PlayerRespawnEvent event) {
+        event.getPlayer().getInventory().addItem(new ItemStack(Material.POTATO, 1));
+    }
+
+    //wofür ist das?
+    @Override
+    public void execute(@NotNull Listener listener, @NotNull Event event) throws EventException {
+
     }
 }
